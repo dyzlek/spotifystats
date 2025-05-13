22dafb128f7ee87c25cdbd4e8307bf4259dbe064
Moxolote <dylanbelledent@gmail.com>
2025-05-13 10:13:28 +0200

    test2

--- script/script.js
+++ script/script.js
@@ -1,195 +1,154 @@
 fetch("data/data.json")
-// Il permet de récupérer un fichier distant
-
 .then(response => response.json())
-// https://reqbin.com/code/javascript/wc3qbk0b/javascript-fetch-json-example
+.then(tracks => {
+  displayTopTracks(tracks);
+  displayTopArtistsChart(tracks);
+  displayGenreDistribution(tracks);
+  displayTopAlbums(tracks);
+});
 
-.then(tracks =>{
-
-    const sortedTracks = [...tracks].sort((a, b) => b.popularity - a.popularity).slice(0, 50);
-
-const tbody = document.querySelector('#trackTable tbody');
-
-sortedTracks.forEach(track => {
-    const row = document.createElement('tr');
+function displayTopTracks(tracks) {
+  const sortedTracks = [...tracks].sort((a, b) => b.popularity - a.popularity).slice(0, 50);
+  const tbody = document.querySelector('#trackTable tbody');
   
-    const titre = `<td data-label="Titre :">${track.name}</td>`;
-    const artiste = `<td data-label="Artiste :">${track.artists.map(a => a.name).join(', ')}</td>`;
-    const album = `<td data-label="Album :">${track.album.name}</td>`;
-    row.innerHTML = titre + artiste + album;
+  sortedTracks.forEach(track => {
+    const row = document.createElement('tr');
+    
+    row.innerHTML = `
+      <td data-label="Titre :">${track.name}</td>
+      <td data-label="Artiste :">${track.artists.map(a => a.name).join(', ')}</td>
+      <td data-label="Album :">${track.album.name}</td>
+    `;
 
     const button = document.createElement("button");
     button.className = "btn btn-primary d-flex align-items-center justify-content-center";
     button.style.width = "78px";
     button.style.height = "31px";
     button.innerHTML = `<i class="bi bi-info-circle me-1" style="font-size: 14px;"></i><span>Détails</span>`;
-    button.addEventListener("click", () => openPopup(track));    
+    button.addEventListener("click", () => openPopup(track));
 
-const td = document.createElement("td");
-td.appendChild(button);
-row.appendChild(td);
-
+    const td = document.createElement("td");
+    td.appendChild(button);
+    row.appendChild(td);
+    
     tbody.appendChild(row);
   });
+}
 
-// Tableau d'objets
-
-    const artistCount = {};
-    // Objet pour stocker le nombre de morceau de chaque artiste
-
-    tracks.forEach(track =>{
-        track.artists.forEach(artist => {
-            const name = artist.name;
-            artistCount[name] = (artistCount[name] || 0) + 1;
-        });
+function displayTopArtistsChart(tracks) {
+  const artistCount = {};
+  
+  tracks.forEach(track => {
+    track.artists.forEach(artist => {
+      const name = artist.name;
+      artistCount[name] = (artistCount[name] || 0) + 1;
     });
-    // Chaque morceau peut avoir plusieurs artistes (feat par exemple), il ne faut pas les oublier
-    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-
-    const sortedArtists = Object.entries(artistCount)
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, 10);
-    // On trie les artistes (de celui qui a le plus de morceaux en premier...) puis on garde les dix premiers
-    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-
-    const labels = sortedArtists.map(entry => entry[0]);
-    const values = sortedArtists.map(entry => entry[1]);
-    const maxValue = Math.max(...values);
-
-    new Chart(document.getElementById('artistChart'),{
-        type: 'bar',
-        // Créer un graphique en barre
-
-        data:{
-            labels: labels,
-            datasets: [{
-                label: "Nombre de morceaux",
-                data: values,
-                backgroundColor: "#0D6EFD"
-            }]
-        },
-        // Données du graphique
-
-        options:{
-            indexAxis: 'y',
-
-            scales:{
-                x:{
-                    max: maxValue,
-                    beginAtZero: true,
-                    title:{
-                        display: true,
-                        text: "Nombre de morceaux",
-                        font:{
-                            size: 14
-                        }
-                    }
-                }
-            },
-
-            plugins: {
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: "Top 10 des artistes (nombre de morceaux)",
-                    font: {
-                        size: 12,
-                        lineHeight: 1.1
-                    }, 
-                    padding: {
-                        top: 10,
-                        bottom: 10
-                    }
-                },
-
-                responsive: true,
-                maintainAspectRatio: false,
-                layout: {
-                    autoPadding: true
-                }
-            },
-
-            responsive: true,
-            maintainAspectRatio: false
+  });
+  
+  const sortedArtists = Object.entries(artistCount)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10);
+  
+  const labels = sortedArtists.map(entry => entry[0]);
+  const values = sortedArtists.map(entry => entry[1]);
+  const maxValue = Math.max(...values);
+  
+  new Chart(document.getElementById('artistChart'), {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Nombre de morceaux",
+        data: values,
+        backgroundColor: "#0D6EFD"
+      }]
+    },
+    options: {
+      indexAxis: 'y',
+      scales: {
+        x: {
+          max: maxValue,
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: "Nombre de morceaux",
+            font: { size: 14 }
+          }
         }
-    });
-
-
-    const genreCount = {};
-
-    tracks.forEach(track => {
-        track.artists.forEach(artist => {
-            if (Array.isArray(artist.genres)) {
-                artist.genres.forEach(genre => {
-                    genreCount[genre] = (genreCount[genre] || 0) + 1;
-                });
-            }
-        });
-    });
-
-    const sortedGenres = Object.entries(genreCount).sort((a, b) => b[1] - a[1]);
-    const topGenres = sortedGenres.slice(0, 7); // Top 7 (doc seven)
-    const autresGenres = sortedGenres.slice(7); // Les autres seront groupés (rip)
-
-    const genreLabels = topGenres.map(([genre]) => genre);
-    const genreValues = topGenres.map(([, count]) => count);
-
-    if (autresGenres.length > 0) {
-        const autresCount = autresGenres.reduce((acc, [, count]) => acc + count, 0);
-        genreLabels.push("Autres");
-        genreValues.push(autresCount);
+      },
+      plugins: {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: "Top 10 des artistes (nombre de morceaux)",
+          font: { size: 12, lineHeight: 1.1 },
+          padding: { top: 10, bottom: 10 }
+        }
+      },
+      responsive: true,
+      maintainAspectRatio: false
     }
+  });
+}
 
-    const backgroundColors = [
-        "#ff91a9", "#72bef1", "#ffdd88", "#81d3d3",
-        "#b794ff", "#ffbc79", "#6cdb9b", "#b5c0c1"
-    ];
-    // Couleurs du camembert (en mode rbg)
-
-    new Chart(document.getElementById('genreChart'), {
-        type: 'pie',
-        // Créer un graphique en camembert (miam)
-
-        data: {
-            labels: genreLabels,
-            datasets: [{
-                data: genreValues,
-                backgroundColor: backgroundColors
-            }]
-        },
-
-        options: {
-            plugins: {
-                title: {
-                    display: true,
-                    text: "Distribution des genres musicaux",
-                    font: {
-                        size: 12,
-                        lineHeight: 1.1
-                    },
-                    padding: {
-                        top: 10,
-                        bottom: 10
-                    }
-                },
-                legend: {
-                    position: 'right',
-                    labels: {
-                        boxWidth: 15,
-                        font: {
-                            size: 11
-                        }
-                    }
-                },
-                responsive: true,
-                layout: {
-                    autoPadding: true
-                }
-            },
-            responsive: true,
-            maintainAspectRatio: false
-        }
+function displayGenreDistribution(tracks) {
+  const genreCount = {};
+  
+  tracks.forEach(track => {
+    track.artists.forEach(artist => {
+      if (Array.isArray(artist.genres)) {
+        artist.genres.forEach(genre => {
+          genreCount[genre] = (genreCount[genre] || 0) + 1;
+        });
+      }
     });
+  });
+  
+  const sortedGenres = Object.entries(genreCount).sort((a, b) => b[1] - a[1]);
+  const topGenres = sortedGenres.slice(0, 7);
+  const autresGenres = sortedGenres.slice(7);
+  
+  const genreLabels = topGenres.map(([genre]) => genre);
+  const genreValues = topGenres.map(([, count]) => count);
+  
+  if (autresGenres.length > 0) {
+    const autresCount = autresGenres.reduce((acc, [, count]) => acc + count, 0);
+    genreLabels.push("Autres");
+    genreValues.push(autresCount);
+  }
+  
+  const backgroundColors = [
+    "#ff91a9", "#72bef1", "#ffdd88", "#81d3d3",
+    "#b794ff", "#ffbc79", "#6cdb9b", "#b5c0c1"
+];
+  
+  new Chart(document.getElementById('genreChart'), {
+    type: 'pie',
+    data: {
+      labels: genreLabels,
+      datasets: [{
+        data: genreValues,
+        backgroundColor: backgroundColors
+      }]
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "Distribution des genres musicaux",
+          font: { size: 12, lineHeight: 1.1 },
+          padding: { top: 10, bottom: 10 }
+        },
+        legend: {
+          position: 'right',
+          labels: { boxWidth: 15, font: { size: 11 } }
+        }
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+}
 
     const topAlbums = [];
 const seenAlbumIds = new Set();
@@ -263,6 +222,7 @@ function openPopup(track) {
     document.getElementById("popup-title").textContent = track.name;
     const durationMinSec = `${Math.floor(track.duration_ms / 60000)}:${String(Math.floor((track.duration_ms % 60000) / 1000)).padStart(2, "0")}`;
     document.getElementById("popup-duration").textContent = durationMinSec;
+<<<<<<< HEAD
     const popularityCell = document.getElementById("popup-track-popularity");
 popularityCell.innerHTML = `
   <div class="d-flex align-items-center">
@@ -273,6 +233,9 @@ popularityCell.innerHTML = `
   </div>
 `;
 
+=======
+    document.getElementById("popup-track-popularity").textContent = `${track.popularity}/100`;
+>>>>>>> parent of 81c48c0 (update popup n1)
     document.getElementById("popup-track-number").textContent = track.track_number;
     document.getElementById("popup-explicit").textContent = track.explicit ? "Oui" : "Non";
     
@@ -285,54 +248,46 @@ popularityCell.innerHTML = `
     const genresContainer = document.getElementById("popup-genres");
     genresContainer.innerHTML = "";
   
-    // Ajouter un badge pour chaque genre
-    allGenres.forEach(genre => {
-      const badge = document.createElement("span");
-      badge.className = "badge bg-secondary rounded-pill me-2"; // Utilisation d'un badge gris (bg-secondary)
-      badge.textContent = genre;
-      genresContainer.appendChild(badge);
-    });
+  allGenres.forEach(genre => {
+    const badge = document.createElement("span");
+    badge.className = "badge bg-secondary rounded-pill me-2";
+    badge.textContent = genre;
+    genresContainer.appendChild(badge);
+  });
   
-    const audio = document.getElementById("popup-audio");
-    audio.src = track.preview_url || "";
-    audio.load();
+  const audio = document.getElementById("popup-audio");
+  audio.src = track.preview_url || "";
+  audio.load();
   
-    document.getElementById("popup-spotify-link").href = `https://open.spotify.com/track/${track.id}`;
+  document.getElementById("popup-spotify-link").href = `https://open.spotify.com/track/${track.id}`;
   
-    const artistContainer = document.getElementById("popup-artists");
-    artistContainer.innerHTML = "";
-    artists.forEach(artist => {
-      const artistCard = document.createElement("div");
-      artistCard.className = "d-flex align-items-center mb-2";
+  const artistContainer = document.getElementById("popup-artists");
+  artistContainer.innerHTML = "";
+  
+  artists.forEach(artist => {
+    const artistCard = document.createElement("div");
+    artistCard.className = "d-flex align-items-center mb-2";
     
-      const img = document.createElement("img");
-      img.src = artist.images[0]?.url || "fallback.jpg";
-      img.alt = `Photo de ${artist.name}`;
-      img.style.width = "50px";
-      img.style.height = "50px";
-      img.style.objectFit = "cover";
-      img.className = "rounded me-2";
-    
-      const info = document.createElement("div");
-      info.innerHTML = `
+    artistCard.innerHTML = `
+      <img src="${artist.images[0]?.url || "fallback.jpg"}" alt="Photo de ${artist.name}"
+           class="rounded me-2" style="width: 50px; height: 50px; object-fit: cover;">
+      <div>
         <div><strong>${artist.name}</strong></div>
-        <div>Popularité : ${artist.popularity}/100 Followers : ${artist.followers.total.toLocaleString()} </div>
-      `;
+        <div>Popularité : ${artist.popularity}/100 Followers : ${artist.followers.total.toLocaleString()}</div>
+      </div>
+    `;
     
-      artistCard.appendChild(img);
-      artistCard.appendChild(info);
-      artistContainer.appendChild(artistCard);
-    });
+    artistContainer.appendChild(artistCard);
+  });
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup-overlay");
+  popup.style.display = "none";
+  
+  const audio = document.getElementById("popup-audio");
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
   }
-  
-  function closePopup() {
-    const popup = document.getElementById("popup-overlay");
-    popup.style.display = "none";
-  
-    const audio = document.getElementById("popup-audio");
-    if (audio) {
-      audio.pause();
-      audio.currentTime = 0;
-    }
-  }
-  
+}
